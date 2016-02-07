ca5a5d4115a0c03926673e3560d3c74d2385bb2f
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-06 22:05:07 -0600

    Added Common Projecst

---
+++ tests/GladNet.PhotonServer.Common.Tests/EmptyTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladNet.PhotonServer.Common.Tests
+{
+	[TestFixture]
+	public static class EmptyTest
+	{
+		[Test]
+		public static void Test()
+		{
+
+		}
+	}
+}
---
+++ tests/RunTests.sh
@@ -0,0 +1,1 @@
+mono ./testrunner/NUnit.Console.3.0.1/tools/nunit3-console.exe ./GladNet.PhotonServer.Common.Tests/bin/Debug/GladNet.PhotonServer.Common.Tests.dll
