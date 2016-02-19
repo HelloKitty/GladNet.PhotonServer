c2908ded16c440a4b6832dbdb2a7247e14d7c2bf
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-19 16:30:16 -0600

    Added IConnectionDetails Adapter

--- src/GladNet.PhotonServer.Server/GladNet Services/PhotonServerIConnectionDetailsAdapter.cs
+++ src/GladNet.PhotonServer.Server/GladNet Services/PhotonServerIConnectionDetailsAdapter.cs
@@ -4,11 +4,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
 
 namespace GladNet.PhotonServer.Server
 {
 	public class PhotonServerIConnectionDetailsAdapter : IConnectionDetails
 	{
+		/// <summary>
+		/// IPAddress of the remote peer.
+		/// </summary>
+		public IPAddress RemoteIP { get; private set; }
 
+		/// <summary>
+		/// Remote port of the peer.
+		/// </summary>
+		public int RemotePort { get; private set; }
+
+		/// <summary>
+		/// Local port the peer is connecting on.
+		/// </summary>
+		public int LocalPort { get; private set; }
+
+		/// <summary>
+		/// Connection ID of the peer. (unique per port)
+		/// </summary>
+		public int ConnectionID { get; private set; }
 	}
 }
