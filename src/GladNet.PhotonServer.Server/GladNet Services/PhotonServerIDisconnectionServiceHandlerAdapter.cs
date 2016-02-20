3a9b8c27c7dcc1ce7a020d7bb0d52520a81e03c5
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-19 19:13:08 -0600

    Added documentation to all files

--- src/GladNet.PhotonServer.Server/GladNet Services/PhotonServerIDisconnectionServiceHandlerAdapter.cs
+++ src/GladNet.PhotonServer.Server/GladNet Services/PhotonServerIDisconnectionServiceHandlerAdapter.cs
@@ -8,17 +8,32 @@ using System.Threading.Tasks;
 
 namespace GladNet.PhotonServer.Server
 {
+	/// <summary>
+	/// Adapter for the <see cref="IDisconnectionServiceHandler"/> interface.
+	/// </summary>
 	public class PhotonServerIDisconnectionServiceHandlerAdapter : IDisconnectionServiceHandler
 	{
+		/// <summary>
+		/// Indicates if the connection is disconnected.
+		/// </summary>
 		public bool isDisconnected { get; private set; }
 
+		/// <summary>
+		/// Publisher of <see cref="OnNetworkDisconnect"/> events to subscribers.
+		/// </summary>
 		public event OnNetworkDisconnect DisconnectionEventHandler;
 
+		/// <summary>
+		/// Creates a new adapter for the <see cref="IDisconnectionServiceHandler"/> interface.
+		/// </summary>
 		public PhotonServerIDisconnectionServiceHandlerAdapter()
 		{
 			isDisconnected = false;
 		}
 
+		/// <summary>
+		/// Disconnects the connection.
+		/// </summary>
 		public void Disconnect()
 		{
 			isDisconnected = true;
--- src/GladNet.PhotonServer.Server/Message/PhotonRequestMessageAdapter.cs
+++ src/GladNet.PhotonServer.Server/Message/PhotonRequestMessageAdapter.cs
@@ -6,10 +6,22 @@ using System.Text;
 
 namespace GladNet.PhotonServer.Server
 {
+	/// <summary>
+	/// Adapter for the <see cref="IRequestMessage"/> interface.
+	/// </summary>
 	public class PhotonRequestMessageAdapter : IRequestMessage
 	{
-		public NetSendable<PacketPayload> Payload { get; private set; }
+		/// <summary>
+		/// The payload of a <see cref="INetworkMessage"/>. Can be sent accross a network.
+		/// <see cref="NetSendable"/> enforces its wire readyness.
+		/// In Photon we just have to push a payload into it to fit the interface.
+		/// </summary>
+		public NetSendable<PacketPayload> Payload { get; }
 
+		/// <summary>
+		/// Creates a new adapter for the <see cref="IRequestMessage"/> interface.
+		/// </summary>
+		/// <param name="payload">Payload to adapt.</param>
 		public PhotonRequestMessageAdapter(PacketPayload payload)
 		{
 			//Not great but in Production we won't use Photon.
