9b45319072952dc850b6fdeeffe295c395622bec
Andrew Blakely <andrew.blakely@ymail.com>
2016-02-29 14:10:19 -0600

    Moved shared code into Common

---
+++ src/GladNet.PhotonServer.Client/IClientNetworkMessageReciever.cs
@@ -0,0 +1,27 @@
+﻿using GladNet.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GladNet.PhotonServer.Client
+{
+	public interface IClientNetworkMessageReciever
+	{
+		/// <summary>
+		/// Called internally when a response is recieved.
+		/// </summary>
+		/// <param name="payload"><see cref="PacketPayload"/> sent by the peer.</param>
+		/// <param name="parameters">Parameters the message was sent with.</param>
+		void OnReceiveResponse(PacketPayload payload);
+
+		/// <summary>
+		/// Called internally when an event is recieved.
+		/// </summary>
+		/// <param name="payload"><see cref="PacketPayload"/> sent by the peer.</param>
+		/// <param name="parameters">Parameters the message was sent with.</param>
+		void OnReceiveEvent(PacketPayload payload);
+
+		void OnStatusChanged(NetStatus status);
+	}
+}
