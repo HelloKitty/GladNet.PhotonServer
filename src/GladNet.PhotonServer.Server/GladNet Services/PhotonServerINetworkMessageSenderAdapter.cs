769415664749cc806530255416a39b1a166c902d
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-17 21:36:43 -0500

    Add missing doc

--- src/GladNet.PhotonServer.Server/GladNet Services/PhotonServerINetworkMessageSenderAdapter.cs
+++ src/GladNet.PhotonServer.Server/GladNet Services/PhotonServerINetworkMessageSenderAdapter.cs
@@ -1,4 +1,5 @@
-﻿using GladNet.Common;
+﻿using Easyception;
+using GladNet.Common;
 using GladNet.Engine.Common;
 using GladNet.Message;
 using GladNet.Payload;
@@ -13,26 +14,54 @@ using System.Threading.Tasks;
 
 namespace GladNet.PhotonServer.Server
 {
+	/// <summary>
+	/// Adapter for the <see cref="INetworkMessageRouterService"/> interface.
+	/// </summary>
 	public class PhotonServerINetworkMessageSenderClientAdapter : INetworkMessageRouterService
 	{
+		/// <summary>
+		/// Internal Photon <see cref="PeerBase"/>.
+		/// </summary>
 		private PeerBase photonPeer { get; }
 
-		private ISerializerStrategy serializerStrategy;
+		/// <summary>
+		/// Serailization strategy to use for outgoing messages.
+		/// </summary>
+		private ISerializerStrategy serializerStrategy { get; }
 
+		/// <summary>
+		/// Creates a new Photon compatible <see cref="INetworkMessageRouterService"/>.
+		/// </summary>
+		/// <param name="peer"><see cref="PeerBase"/> to route messages to.</param>
+		/// <param name="serializer">Serialization strategy to use for outgoing messages.</param>
 		public PhotonServerINetworkMessageSenderClientAdapter(PeerBase peer, ISerializerStrategy serializer)
 		{
+			Throw<ArgumentNullException>.If.IsNull(peer)?.Now(nameof(peer));
+			Throw<ArgumentNullException>.If.IsNull(serializer)?.Now(nameof(serializer));
+
 			photonPeer = peer;
 			serializerStrategy = serializer;
 		}
 
+		/// <summary>
+		/// Indicates if the <see cref="OperationType"/> can be sent.
+		/// </summary>
+		/// <param name="opType">Operation type.</param>
+		/// <returns>True if the <paramref name="opType"/> can be sent.</returns>
 		public bool CanSend(OperationType opType)
 		{
-			if (opType == OperationType.Event || opType == OperationType.Response)
-				return true;
-			else
-				return false;
+			return opType == OperationType.Event || opType == OperationType.Response;
 		}
 
+		/// <summary>
+		/// Attempts to send a message using the provided <see cref="PacketPayload"/>.
+		/// </summary>
+		/// <param name="opType">The outgoing <see cref="OperationType"/>.</param>
+		/// <param name="payload">The <see cref="PacketPayload"/> to send with the message.</param>
+		/// <param name="deliveryMethod">Delivery method to use for the message.</param>
+		/// <param name="encrypt">Indicates if the message should be encrypted.</param>
+		/// <param name="channel">Channel to send on.</param>
+		/// <returns>Indicates the status of the outgoing message attempt.</returns>
 		public GladNet.Common.SendResult TrySendMessage(OperationType opType, PacketPayload payload, DeliveryMethod deliveryMethod, bool encrypt = false, byte channel = 0)
 		{
 			Photon.SocketServer.SendResult result;
@@ -76,6 +105,14 @@ namespace GladNet.PhotonServer.Server
 			}
 		}
 
+		/// <summary>
+		/// Sends a Photon event message with the provided parameters
+		/// </summary>
+		/// <param name="payload">Payload to send with the message.</param>
+		/// <param name="unreliable">Indicates if the message should be sent unreliabily.</param>
+		/// <param name="encrypt">Indicates if the message should be encrypted.</param>
+		/// <param name="channel">Channel to send the message on.</param>
+		/// <returns>Indicates the result of sending the message.</returns>
 		private Photon.SocketServer.SendResult SendEvent(PacketPayload payload, bool unreliable, bool encrypt, byte channel)
 		{
 			//Builds the message in a context that Photon understands (dictionary of objects)
@@ -85,6 +122,14 @@ namespace GladNet.PhotonServer.Server
 			return photonPeer.SendEvent(data, new SendParameters() { ChannelId = channel, Encrypted = encrypt, Unreliable = unreliable });
 		}
 
+		/// <summary>
+		/// Sends a Photon response message with the provided parameters
+		/// </summary>
+		/// <param name="payload">Payload to send with the message.</param>
+		/// <param name="unreliable">Indicates if the message should be sent unreliabily.</param>
+		/// <param name="encrypt">Indicates if the message should be encrypted.</param>
+		/// <param name="channel">Channel to send the message on.</param>
+		/// <returns>Indicates the result of sending the message.</returns>
 		private Photon.SocketServer.SendResult SendResponse(PacketPayload payload, bool unreliable, bool encrypt, byte channel)
 		{
 			//Builds the message in a context that Photon understands (dictionary of objects)
@@ -94,11 +139,22 @@ namespace GladNet.PhotonServer.Server
 			return photonPeer.SendOperationResponse(data, new SendParameters() { ChannelId = channel, Encrypted = encrypt, Unreliable = unreliable });
 		}
 
+		/// <summary>
+		/// Serializes the <see cref="PacketPayload"/> provided.
+		/// </summary>
+		/// <param name="payload">Payload to serialize.</param>
+		/// <returns>The serialized packet payload.</returns>
 		private byte[] SerializePayload(PacketPayload payload)
 		{
 			return serializerStrategy.Serialize(payload);
 		}
 
+		/// <summary>
+		/// Attempts to send a message using the provided <see cref="PacketPayload"/>.
+		/// </summary>
+		/// <param name="opType">The outgoing <see cref="OperationType"/>.</param>
+		/// <param name="payload">The <see cref="PacketPayload"/> to send with the message.</param>
+		/// <returns>Indicates the status of the outgoing message attempt.</returns>
 		public GladNet.Common.SendResult TrySendMessage<TPacketType>(OperationType opType, TPacketType payload) 
 			where TPacketType : PacketPayload, IStaticPayloadParameters
 		{
